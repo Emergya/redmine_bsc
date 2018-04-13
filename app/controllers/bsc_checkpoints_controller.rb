5b85f27b585ff804197c900a9c0b659a521a6fce
jresinas <jresinas@emergya.com>
2018-04-13 14:17:41 +0200

    Avoid create/update target_incomes and target_expenses fields for no admin users

--- app/controllers/bsc_checkpoints_controller.rb
+++ app/controllers/bsc_checkpoints_controller.rb
@@ -102,7 +102,11 @@ class BscCheckpointsController < ApplicationController
   end
 
   def checkpoint_params
-    params.require(:checkpoint).permit(:project_id, :author_id, :description, :checkpoint_date, :scheduled_finish_date, :held_qa_meetings, :base_line, :target_expenses, :target_incomes, :achievement_percentage, bsc_checkpoint_efforts_attributes: [:id, :hr_profile_id, :scheduled_effort, :number])
+    if User.current.admin?
+      params.require(:checkpoint).permit(:project_id, :author_id, :description, :checkpoint_date, :scheduled_finish_date, :held_qa_meetings, :base_line, :target_expenses, :target_incomes, :achievement_percentage, bsc_checkpoint_efforts_attributes: [:id, :hr_profile_id, :scheduled_effort, :number])
+    else
+      params.require(:checkpoint).permit(:project_id, :author_id, :description, :checkpoint_date, :scheduled_finish_date, :held_qa_meetings, :base_line, :achievement_percentage, bsc_checkpoint_efforts_attributes: [:id, :hr_profile_id, :scheduled_effort, :number])
+    end
   end
 
   def has_bsc_project_info
